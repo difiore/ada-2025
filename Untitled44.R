816778f1fe8343c94feac903e6f9d51f736fd858
difiore <anthony.difiore@gmail.com>
2025-01-30 14:18:56 -0600

    refresh exercise 3, modules 9 and 10

--- Untitled44.R
+++ Untitled44.R
@@ -1,49 +1,57 @@
-# base R
-f <- "~/Downloads/CPDS-1960-2014-reduced.csv"
-d1 <- read.csv(f, header = TRUE)
-glimpse(d1)
-is.factor(d1$country)
-
-f <- "~/Downloads/CPDS-1960-2014-reduced.txt"
-d2 <- read.delim(f, header = TRUE, sep = "\t")
-glimpse(d1)
-
-# tidyverse/readr
 library(tidyverse)
-f <- "~/Downloads/CPDS-1960-2014-reduced.csv"
-d3 <- read_csv(f, col_names = TRUE)
-glimpse(d3)
 
-f <- "~/Downloads/CPDS-1960-2014-reduced.txt"
-d4 <-read_delim(f, col_names = TRUE, delim = "\t")
-glimpse(d4)
+f <-
+  "https://raw.githubusercontent.com/difiore/ada-datasets/refs/heads/main/KamilarAndCooperData.csv"
+d <-
+  read_csv(f, col_names = TRUE) # creates a "tibble"
 
-f <- "https://raw.githubusercontent.com/difiore/ada-datasets/main/CPDS-1960-2014-reduced.csv"
-d5 <- read_csv(f, col_names = TRUE)
-glimpse(d5)
+dim(d)
+head(d)
+tail(d)
+str(d)
+glimpse(d)
+dim(d)
+names(d) # or colnames(d)
+rownames(d)
 
-# readxl
-library(readxl)
-f <- "~/Downloads/CPDS-1960-2014-reduced.xlsx"
-d6 <-read_excel(f, sheet = 1, col_names = TRUE)
-glimpse(d6)
+attach(d)
+mean(Rest, na.rm = TRUE)
+with(d, mean(Brain_Size_Female_Mean, na.rm = TRUE))
+detach(d)
+detach(package:kableExtra, unload = TRUE)
+read_csv(f)
+library(readr)
+read_csv(f)
+summary(d)
 
-# decimal points get truncated when saving from excel to csv!
+library(skimr)
+skim(d)
 
-library(rdrop2)
-drop_auth(new_user = TRUE)  # opens a browser dialog box to ask for authorization...
-drop_dir()  # lists the contents of your dropbox folder
-f <- "CPDS-1960-2014-reduced.csv"  # name of the file to read from
-f <- drop_search(query = f, mode = "filename")
-# searches your dropbox directory for file or directory names... this can be
-# slow!
-filenames <- vector()
-for (i in 1:length(f$matches)) {
-  filenames <- c(filenames, f$matches[[i]]$metadata$path_display)
-  # this is the location of the results returned above the [[i]] returns each
-  # encountered file with a matching filename and puts them into a vector
-}
-d <- drop_read_csv(filenames[1], header = TRUE, sep = ",", stringsAsFactors = FALSE)
-# here the [1] reads only the first file from filenames, but this can be
-# modified this to read more than one file
-detach(package:rdrop2)
+attach(d)
+boxplot(log(Body_mass_female_mean))
+stripchart(log(d$Body_mass_female_mean),
+             method = "jitter",
+             col = "blue",
+             vertical = TRUE,
+             add = TRUE)
+
+p <- ggplot(
+  data = d,
+  aes(x = "", y = log(Body_mass_female_mean))
+  ) +
+  geom_boxplot(na.rm = TRUE)
+
+p <- ggplot(
+  data = d,
+  aes(x = Family, y = log(Body_mass_female_mean))
+) +
+  geom_boxplot(na.rm = TRUE) +
+  geom_jitter(color = "blue", width = 0.1)
+p
+
+p <- ggplot(data = d, aes(
+  x = log(Body_mass_female_mean),
+  y = log(Brain_Size_Female_Mean)
+  )
+) + geom_point()
+
